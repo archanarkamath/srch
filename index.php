5eb2334120de9aa0f75573b302f8296c0a7df680
archanarkamath <rarchana.archana95@gmail.com>
2019-10-26 13:50:26 +0530

    dashboard changes

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
  *
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- dash
  */
 
 use Drupal\Core\DrupalKernel;
