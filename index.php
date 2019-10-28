5ad1aa71852fd2085d58dffb757ec654eee129f8
sumeetbiswal <biswal.sumeet@gmail.com>
2019-10-28 11:42:10 -0700

    byee

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  *
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- dash
+ Byeeee
  */
 
 use Drupal\Core\DrupalKernel;
