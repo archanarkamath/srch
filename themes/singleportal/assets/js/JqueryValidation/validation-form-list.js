85198f8d6e0a68e53a1e2cdb3a8b55a44d4261a2
Sumeet Biswal <1364945@TCS.com>
2020-04-22 12:03:09 +0530

    delete all stuff

--- themes/singleportal/assets/js/JqueryValidation/validation-form-list.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $("#multistep-form-one").validationEngine();
-   $("#multistep-form-two").validationEngine();
-   $("#multistep_form_three").validationEngine();
-   $("#multistep-form-four").validationEngine();
-   $("#branch-form").validationEngine();
-   $("#company-form").validationEngine();
-});
