6b1cd838cb1a6316957d8cc6292c6731ed477673
Sumeet Biswal <1364945@TCS.com>
2020-04-26 17:30:28 +0530

    dashboard slider along with minor changes

--- modules/custom/dashboard/src/Plugin/Block/EmployeelistCount.php
+++ modules/custom/dashboard/src/Plugin/Block/EmployeelistCount.php
@@ -2,10 +2,7 @@
 
 namespace Drupal\dashboard\Plugin\Block;
 
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountInterface;
 
 /**
  * Provides a block with a simple text.
@@ -25,15 +22,8 @@ class EmployeelistCount extends BlockBase {
 	$count = $emp_obj->getEmployeeCount();
 	
     return [
-      '#markup' => $this->t('<a href="'.$base_url.'/employee"><div class="col-lg-3 col-sm-6 col-xs-12">
-                                <div class="white-box" style="box-shadow: 0 4px 8px 0 grey;">
-                                    <h3 class="box-title">Employees</h3>
-                                    <ul class="list-inline two-part">
-                                        <li><i class="icon-people text-purple"></i></li>
-                                        <li class="text-right"><span class="counter">'.$count.'</span></li>
-                                    </ul>
-                                </div>
-                            </div></a>'),
+	  '#theme' => 'employee_count_card',
+	  '#emp_count' =>	$count
     ];
   }
 
