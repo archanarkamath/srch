75dd2b4431b7136dc0c5f81bd1b555c35fd3fe7b
archanarkamath <rarchana.archana95@gmail.com>
2020-06-11 18:12:57 +0530

    leave module basic setup

--- modules/custom/leave/src/Controller/Leave.php
+++ modules/custom/leave/src/Controller/Leave.php
@@ -2,21 +2,25 @@
 
 namespace Drupal\leave\Controller;
 
+
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
-use Drupal\library\Controller\Encrypt;
+use Drupal\Core\Form\FormStateInterface;
 
 class Leave extends ControllerBase {
  public function leavelist() {
+  
+  global $base_url;
+  $asset_url = $base_url.'/'.\Drupal::theme()->getActiveTheme()->getPath();
 
- $element['display']['Leavelist'] = array(
+  $element['display']['Leavelist'] = array(
       '#type' 	    => 'table',
-      '#header' 	  =>  array(t('Request Type.'), t('Leave type'),t('Start date'), t('End date'),  t('Number of days'), t('Submit'), t('Action')),
+      '#header' 	  =>  array(t('Request Type.'), t('Leave request'), t('Leave type'),t('Start date'), t('End date'),  t('Number of days'), t('Status'), t('Submit date')),
       '#rows'		    =>  $rows,
       '#attributes' => ['class' => ['table text-center table-hover table-striped table-bordered dataTable'], 'style'=>['text-align-last: center;']],
       '#prefix'     => '<div class="panel panel-info">
-                        <h3 class="box-title col-md-10">Leave Request</h3>
+                        <h3 class="box-title col-md-10">Leave List</h3>
                        <div class=" col-md-2">
                         <a href="#" id="exportit" data-toggle="tooltip" data-original-title="Word Document"><img src="'.$asset_url.'/assets/images/icon/word.png" /></a> &nbsp;
 						<a href="'.$base_url.'/branch/export/excel" data-toggle="tooltip" data-original-title="Excel"><img src="'.$asset_url.'/assets/images/icon/excel.png" /></a> &nbsp;
@@ -29,7 +33,7 @@ class Leave extends ControllerBase {
                         <hr>
                         <div id="editable-datatable_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
                         <div class="row"><div class="col-sm-6"><a href ="#"><span  type="button" class="btn btn-info" style="background-color: #4c5667">
-                        <i class="mdi mdi-plus"></i> Apply Leave </span></a></div> <br><br><br></div></div><div class="row"><div class="col-sm-12">',
+                         Apply Leave </span></a></div> <br><br><br></div></div><div class="row"><div class="col-sm-12">',
       '#suffix'     => '</div></div></div></div></div></div>',
 	  '#empty'		=>	'No Leaves applied yet.'
     );
